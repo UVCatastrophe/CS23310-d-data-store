58c04bbdca870664d88f58a767d2c0bd0e5bebfd
UVCatastrophe <ramici@uchicago.edu>
2014-05-26 13:35:16 -0500

    raft data_structures

--- data-store.py
+++ data-store.py
@@ -1,11 +1,27 @@
 import zmq
 
-#The state for the current process
+#The state for the currect process' sockets
 proc = process()
+#The state for the current process' RAFT_instance
+raft = RAFT_instance()
 #Dictionary which maps keys to values once they have been comitted
 data_store = {}
 
-#Class which contains the necessary state for the process to run
+#Class with all the state necessary for an instance of RAFT
+class RAFT_instance:
+    def __init__(self):
+        self.currentTerm = 0 #The latest term the server has seen (monotonicly increasing)
+        self.votedFor = None #The id of the last candidate vvoted for
+        self.log = [] #A list of log entries 4-tuple (Log#,command,key,value)
+
+        self.commitIndex #Index into the log of the highest log entry that has been commited
+        self.lastApplied #the index of the highest log entry applied
+
+        #State used when the process is a leader
+        self.nextIndex = [] #index of the next log entry to send to each server
+        self.matchIndex = [] #index of the highest log enty which has been replicated on each server
+
+#Class which contains the necessary state for the process to connect over a socket
 class process:
     def __init__(self):
         return
@@ -25,18 +41,34 @@ class process:
     def read_message(self):
         return self.socketRecv.recv()
 
-#The class which contains the relavent information to run
-# a round of paxos.
-class paxos_message:
-    def __init__(self,instr_num, seq_num, msg_type,key,val,sender,recpt, prior=None):
-        self.instr_num = instr_num #The identifier for the paxos instnace
-        self.seq_num = seq_num #The number for the sequnce within a paxos instance
-        self.msg_type = msg_type #The type of the paxos message. See "Types"
-        self.key = key #Key to read/write to the data store?
-        self.val = val #Val to read/write to the data strore?
-        self.sender = sender #Id of the person who sent the message
-        self.recpt = recpt #Id of the recipt. Should be the ID of the current process
-        self.prior = prior #Used for certain replies from acceptors
+#The class which contains the information relevent to append a new entry to the log
+class append_message:
+    def __init__(self,term, leader, prevLogIndex,prevLogTerm,log_entries,leaderCommit,sender,recpt):
+        self.term = term #The term of the current leader
+        self.leader = leader #The id of the current leader
+        self.prevLogIndex = prevLogIndex #index of the log entry precieding "log_entries"
+        self.prevLogTerm = prevLogTerm #term corresponding to preLogIndex
+        self.entries = log_entries #entries to be appended to the log. See above for format
+        self.leaderCommit = leaderCommit #The commit index of the leader
+        self.sender = sender #Should be the leader.
+        self.recpt = recpt #Should be the same id as the process
+
+#Sent by a candidate to request a vote
+class requestVote_message:
+    def __init__(self,term,candidate,lastLogIndex,lastLogTerm,sender,recpt):
+        self.sender = sender
+        self.recpt = recpt
+        self.term = term #The term of the candidate
+        self.candidate = candidate #The id of the candidate requesting a vote
+        self.lastLogIndex = lastLogIndex #The index of the candidates last log entry
+        self.lastLogTerm = lastLogTerm #The term of the candidates last log entry
+
+class replyVote_message:
+    def __init__(self, term, voteGranted,sender,recpt):
+        self.sender = sender
+        self.recpt = recpt
+        self.term = term #The term of the election request
+        self.voteGranted = voteGranted #True or False if the vote was granted or not
 
 #A message used for Byzantine generals message passing
 class byzantine_message:
@@ -45,9 +77,13 @@ class byzantine_message:
         self.recpt = recpt #The recipient. Should be the same as the process
         self.frame = frame #The value to be decided upon. Value for small values, checksum for large values
 
-#A message, probably sent by a user?
+#A message, probably sent by a user
 class transaction_message:
-    def __init__(self):
+    def __init__(self,key,val,sender,recpt):
+        self.key = key
+        self.val = val
+        self.sender = sender
+        self.recpt = recpt
         return
 
 #--The main message loop--
